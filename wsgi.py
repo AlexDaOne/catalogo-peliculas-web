c5ec0fa68fc8ad26c2dd39bf72f45a8f3ae434b2
AlexDaOne <alexandernadalesmartinez@gmail.com>
2026-06-09 01:10:29 -0600

    Fix ruta absoluta para index.html

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
+import os
 from api.peliculas import app
 from flask import send_from_directory
 
+# Obtenemos la carpeta base donde está wsgi.py
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 @app.route('/')
 def home():
-    return send_from_directory('public', 'index.html')
+    # Buscamos index.html en la carpeta 'public' que está al lado de wsgi.py
+    public_folder = os.path.join(BASE_DIR, 'public')
+    return send_from_directory(public_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run()
