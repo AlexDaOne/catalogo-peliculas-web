dc5783f3cd0bf0d6746d42c55f4fdc8a6542d0fc
AlexDaOne <alexandernadalesmartinez@gmail.com>
2026-06-09 01:08:42 -0600

    Fix rutas Vercel y serve index.html

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from api.peliculas import app
+from flask import send_from_directory
+
+@app.route('/')
+def home():
+    return send_from_directory('public', 'index.html')
 
 if __name__ == "__main__":
     app.run()
